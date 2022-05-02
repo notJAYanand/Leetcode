0acd1a91f0b05e0a7e506de5de7be1532841312d
Jay Anand <53784796+notJAYanand@users.noreply.github.com>
2022-05-02 12:33:59 +0530

    Time: 15 ms (47.89%), Space: 16.3 MB (25.41%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cpp
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -1,24 +1,41 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        vector<int> res;
-        int n= nums.size();
-        for(int i=0;i<n;i++)
+        //SIGMA MALE TECHNIQUE
+        int i=0;
+        int j=nums.size()-1;
+        while(i<j)
         {
+            if(nums[i]%2>nums[j]%2)
+            {
+                swap(nums[i],nums[j]);
+            }
             if(nums[i]%2==0)
-            {
-                res.push_back(nums[i]);
-            }
+                i++;
+            if(nums[j]%2!=0)
+                j--;
+        }
+        return nums;
+        
+        // PUSSY TECHNIQUE
+//         vector<int> res;
+//         int n= nums.size();
+//         for(int i=0;i<n;i++)
+//         {
+//             if(nums[i]%2==0)
+//             {
+//                 res.push_back(nums[i]);
+//             }
             
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]%2!=0)
-            {
-                res.push_back(nums[i]);
-            }
+//         }
+//         for(int i=0;i<n;i++)
+//         {
+//             if(nums[i]%2!=0)
+//             {
+//                 res.push_back(nums[i]);
+//             }
 
-        }
-        return res;
+//         }
+//         return res;
     }
 };
