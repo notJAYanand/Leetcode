80268aeae973f56e28fc6772706be3f65074ab12
Jay Anand <53784796+notJAYanand@users.noreply.github.com>
2022-05-04 11:23:42 +0530

    Time: 251 ms (25.30%), Space: 73 MB (16.47%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,49 +1,41 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        // unordered_map<int,int> mp;
+        unordered_map<int,int> mp;
         int count=0;
-//         for(int i=0;i<nums.size();i++)
-//         {
-//             for(int j=i+1;j<nums.size();j++)
-//             {
-//                 if(nums[i]+nums[j]==k)
-//                 {
-//                     count++;
-//                     nums[i]=-1;
-//                     nums[j]=-1;
-//                 }
-                
-//             }
-//         }
-        int l=0; 
-        int r=nums.size()-1;
-        sort(nums.begin(),nums.end());
-        while(l<r)
+        for(int i=0;i<nums.size();i++)
         {
-            if(nums[l]+nums[r]>k)
-            {
-                r--;
-            }
-            else if(nums[l]+nums[r]<k)
-            {
-                l++;
-            }
-            else if(nums[l]+nums[r]==k)
+            int dif=k-nums[i];
+            if(mp[dif]>0)
             {
                 count++;
-                l++;
-                r--;
-                // nums[l]=INT_MIN;
-                // nums[r]=INT_MIN;
-                // nums.erase(l);
-                // nums.erase(r);
-                
-                // l=0;
-                // r=nums.size()-1;
-                // sort(nums.begin(),nums.end());
+                mp[dif]--;
+            }
+            else
+            {
+                mp[nums[i]]++;
             }
         }
+        // int l=0; 
+        // int r=nums.size()-1;
+        // sort(nums.begin(),nums.end());
+        // while(l<r)
+        // {
+        //     if(nums[l]+nums[r]>k)
+        //     {
+        //         r--;
+        //     }
+        //     else if(nums[l]+nums[r]<k)
+        //     {
+        //         l++;
+        //     }
+        //     else if(nums[l]+nums[r]==k)
+        //     {
+        //         count++;
+        //         l++;
+        //         r--;
+        //     }
+        // }
        return count; 
     }
 };
