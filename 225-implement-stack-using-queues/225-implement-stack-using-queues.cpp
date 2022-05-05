a0a7317ee406cb47a2ed8e2b023c87b779e66f6d
Jay Anand <53784796+notJAYanand@users.noreply.github.com>
2022-05-05 16:11:18 +0530

    Time: 5 ms (13.56%), Space: 6.9 MB (58.19%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -1,7 +1,9 @@
 class MyStack {
 public:
+    //1 queue is used rest commented lines is for 2 queue implementation
     queue<int>q1;
-    queue<int>q2;
+    // queue<int>q2;
+    int peek=-1;
         
     int topElement;
     MyStack() {
@@ -9,23 +11,37 @@ public:
     }
     
     void push(int x) {
-        q2.push(x);
-        while(!q1.empty())
-        {
-            q2.push(q1.front());
-            q1.pop();
-        }
-        swap(q1,q2);
+        peek=x;
+        q1.push(x);
+        // q2.push(x);
+        // while(!q1.empty())
+        // {
+        //     q2.push(q1.front());
+        //     q1.pop();
+        // }
+        // swap(q1,q2);
     }
     
     int pop() {
-        int x=q1.front();
+        int n= q1.size()-1;
+        while(n)
+        {
+            peek=q1.front();
+            q1.push(peek);
+            q1.pop();
+            n--;
+        }
+        int res=q1.front();
         q1.pop();
-        return x;
+        return res;
+        // int x=q1.front();
+        // q1.pop();
+        // return x;
     }
     
     int top() {
-        return q1.front();
+        return peek ;
+        // return q1.front();
     }
     
     bool empty() {
