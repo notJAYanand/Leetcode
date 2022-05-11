8781aa5af10728fe818c7569b9c1e9fc2c2f844e
Jay Anand <53784796+notJAYanand@users.noreply.github.com>
2022-05-11 09:10:37 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (58.60%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -3,4 +3,8 @@ public:
     int countVowelStrings(int n) {
         return (n+4)*(n+3)*(n+2)*(n+1) / 24;
     }
+    //  n     a   e   i   o   u
+    //  1     1   1   1   1   1 = 5
+    //  2     5   4   3   2   1 = 15
+    //  3     15  10  6   3   1 = 30
 };
