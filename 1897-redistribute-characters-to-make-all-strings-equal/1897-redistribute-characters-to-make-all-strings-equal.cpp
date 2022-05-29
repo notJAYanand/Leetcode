5e991d81ee7f551fa335b948bb0853b148cff809
Jay Anand <53784796+notJAYanand@users.noreply.github.com>
2022-05-29 12:42:22 +0530

    Time: 12 ms (94.07%), Space: 11.6 MB (82.77%) - LeetHub

--- 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
@@ -20,8 +20,8 @@ public:
             }
             else
             { 
-                res=false;
-                break;
+                return false;
+                
             }
         }
         return res;
