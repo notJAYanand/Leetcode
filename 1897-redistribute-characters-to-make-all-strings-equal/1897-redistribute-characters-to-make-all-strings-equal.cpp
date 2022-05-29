c8feae8252d84d1c74b09d3c9e1006f6cdcbc97f
Jay Anand <53784796+notJAYanand@users.noreply.github.com>
2022-05-29 12:05:42 +0530

    Time: 39 ms (10.45%), Space: 11.5 MB (82.77%) - LeetHub

---
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+        vector<int> count(26);
+        int n=words.size();
+        for(int i=0;i<n;i++)
+        {
+            for(auto c: words[i])
+            {
+                count[c-'a']++;
+            }
+        }
+        bool res=false;
+    
+        for(int i=0;i<26;i++)
+        {
+            if(count[i]%n==0)
+            {
+                res=true;
+            }
+            else
+            { 
+                res=false;
+                break;
+            }
+        }
+        return res;
+    }
+};
