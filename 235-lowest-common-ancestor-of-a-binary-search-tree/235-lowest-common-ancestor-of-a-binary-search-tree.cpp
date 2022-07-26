4ba7f00528fbb22f95328fee2306f4647c1f79e3
Jay Anand <53784796+notJAYanand@users.noreply.github.com>
2022-07-26 22:58:28 +0530

    Time: 43 ms (57.41%), Space: 23.2 MB (63.67%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,14 +10,28 @@
 
 class Solution {
 public:
+//     O(n)
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         if(root==NULL|| root==p|| root==q) return root;
+        
+//         TreeNode* left=lowestCommonAncestor(root->left,p,q);
+//         TreeNode* right=lowestCommonAncestor(root->right,p,q);
+        
+//         if(left==NULL) return right;
+//         else if(right==NULL) return left;
+//         else return root;
+//     }
+    //O(n)
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==NULL|| root==p|| root==q) return root;
-        
-        TreeNode* left=lowestCommonAncestor(root->left,p,q);
-        TreeNode* right=lowestCommonAncestor(root->right,p,q);
-        
-        if(left==NULL) return right;
-        else if(right==NULL) return left;
-        else return root;
-    }
+            TreeNode *current = root;
+            while(current != NULL)
+            {
+                if( (p->val < current->val) && (q->val < current->val) )
+                    current = current-> left;
+                else  if( (p->val > current->val) && (q->val > current->val) )
+                    current = current-> right;
+                else return current;
+            }
+            return root;
+        }
 };
