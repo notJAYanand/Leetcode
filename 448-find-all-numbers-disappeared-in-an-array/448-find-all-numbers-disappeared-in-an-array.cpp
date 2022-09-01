ebef91ddb354a4717065594333a147d24ca07d05
Jay Anand <53784796+notJAYanand@users.noreply.github.com>
2022-09-01 11:54:37 +0530

    Time: 86 ms (66.21%), Space: 33.7 MB (83.82%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        // sort(nums.begin(),nums.end());
         vector<int>res;
-        // unordered_map<int,int>mp;
-        unordered_set<int>mp;
-        int n=nums.size();
-
-        for(auto i: nums)
-        {
-            // mp[i]++;
-            mp.insert(i);
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(mp.find(i+1)==mp.end())
-            {
-                res.push_back(i+1);
+        for(int i=0;i<nums.size();i++) {
+            int m=abs(nums[i])-1; // to convert into 0 index
+            if(nums[m]>0) {
+                nums[m]=-(nums[m]);
+            }
+            else {
+                nums[m]=nums[m];
             }
         }
+        for(int i=0;i<nums.size();i++) {
+            if(nums[i]>0) res.push_back(i+1);
+        }
+        return res;
+        
+//         vector<int>res;
+//         // unordered_map<int,int>mp;
+//         unordered_set<int>mp;
+//         int n=nums.size();
+
+//         for(auto i: nums)
+//         {
+//             // mp[i]++;
+//             mp.insert(i);
+//         }
+//         for(int i=0;i<n;i++)
+//         {
+//             if(mp.find(i+1)==mp.end())
+//             {
+//                 res.push_back(i+1);
+//             }
+//         }
         
 
-        return res;
+//         return res;
     }
 };
