06543b0cf0a614e6eb3b7092c7f0f9927e78610f
Jay Anand <53784796+notJAYanand@users.noreply.github.com>
2024-07-02 20:02:57 +0530

    Time: 4 ms (59.42%), Space: 12.7 MB (53.60%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -8,18 +8,10 @@ public:
         int n2=nums2.size();
         
         vector<int> result;
-        
-        // for(int i=0;i<n1;i++) {
-        //     for(int j=0;j<n2;j++) {
-        //         if(nums1[i]==nums2[j]) {
-        //             result.push_back(nums1[i]);
-        //             break;
-        //         }
-        //     }
-        // }
-        // int k=0;
+
         int i=0;
         int j=0;
+        int k=0;
         while(i<n1 && j<n2) {
             if(nums1[i]<nums2[j]) {
                 i++;
@@ -28,10 +20,12 @@ public:
                 j++;
             }
             else{
-                result.push_back(nums1[i]);
+                // result.push_back(nums1[i]); //its using space, we can just return 1 of the arrays
+                nums1[k++]=nums1[i];
                 i++;j++;
             }
         }
-        return result;
+        // return result;
+        return vector<int>(nums1.begin(),nums1.begin()+k);
     }
 };
