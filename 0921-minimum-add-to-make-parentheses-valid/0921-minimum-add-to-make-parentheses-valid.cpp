95b6f0c1ef70227d31c9ffd178aa53d9261a9416
Jay Anand <53784796+notJAYanand@users.noreply.github.com>
2024-10-09 09:57:56 +0530

    Time: 3 ms (42.94%), Space: 7.8 MB (81.46%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        stack<char> stack;
+        // stack<char> stack;
+        int stackSize=0;
         int nAdd=0;
         for(auto i: s) {
             if(i=='(') {
-                stack.push(i);
+                // stack.push(i);
+                stackSize++;
             }
             else if(i==')'){
-                if(stack.empty()) {
+                // if(stack.empty()) {
+                if(stackSize==0) {
                     nAdd++;
                 }
                 else {
-                    stack.pop();
+                    // stack.pop();
+                    stackSize--;
                 }
             }
         }
-        return nAdd+stack.size();
+        // return nAdd+stack.size();
+        return nAdd+stackSize;
     }
 };
